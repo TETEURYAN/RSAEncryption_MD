7b1702b17399f36f52fc1dd5a836504e99f4f449
TETEURYAN <91018438+TETEURYAN@users.noreply.github.com>
2022-11-28 00:47:46 -0300

    Keys

--- Keys_generator_RSA.c
+++ Keys_generator_RSA.c
@@ -23,6 +23,83 @@ bool mdc (unsigned long long a, unsigned long long b) {
    return a == 1 ? true : false;
 }
 
+unsigned long long int potencia(unsigned long long int a, unsigned long long int e, unsigned long long int n)
+{
+
+    unsigned long long int chave_privada_D = a, P = 1, chave_publica_E = e;
+
+    while (1)
+    {
+
+        if (chave_publica_E == 0)
+            return P;
+
+        else if (chave_publica_E % 2 != 0)
+        {
+
+            P = (chave_privada_D * P) % n;
+            chave_publica_E = (chave_publica_E - 1) / 2;
+        }
+
+        else
+        {
+            chave_publica_E = chave_publica_E / 2;
+        }
+
+        chave_privada_D = (chave_privada_D * chave_privada_D) % n;
+    }
+}
+bool mdc (unsigned long long int a, unsigned long long int b) {
+    while (b != 0)
+    {
+       int r = a % b;
+       a = b;
+       b = r;
+    }   
+   return a == 1 ? true : false;
+}
+
+int indice_d(unsigned long long int e, unsigned long long int phi){
+  unsigned long long int b_base = phi, q, t;
+  unsigned long long int x0 = 0, chave_privada = 1;
+
+  if(phi == 1) return 1;
+
+  while(e > 1){
+    q = e/phi;
+    t = phi;
+    phi = e%phi;
+    e = t;
+    t = x0;
+    x0 = chave_privada - (q*x0);
+    chave_privada = t;  
+  }
+
+  if(chave_privada <0)
+  {
+    chave_privada += b_base;
+  } 
+  return chave_privada;
+}
+
+bool ehPrimo (unsigned long long int p) {
+
+    unsigned long long int i;
+    double j;
+    
+    if(p < 2)
+        return false;
+
+    j = sqrt(p);
+
+    for (i = 2; i <= j; i++) {
+        if (p % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
 void critpografando_mensagem(lli frase_enumerada[], int tamanho, lli frase_criptograda[], int e, int n)
 {
     for(int i =0; i<tamanho-1; i++)
@@ -34,6 +111,31 @@ void critpografando_mensagem(lli frase_enumerada[], int tamanho, lli frase_cript
         }
         return;
 }
+
+unsigned long long int exponenciacao_modular_rapida(unsigned long long int denominador, unsigned long long int potencia, unsigned long long int mod)
+{
+    unsigned long long int n = denominador;
+    unsigned long long int k = potencia;
+    unsigned long long int d = mod;
+    unsigned long long int resultado, pot;
+
+    pot = n % d;
+    resultado = 1;
+
+    for ( ; k > 0; k /= 2) 
+    {
+        if (k % 2 == 1) 
+        {
+            resultado = (resultado * pot) % d;
+        }
+    
+        pot = (pot * pot) % d;
+     }
+    
+      return resultado;
+}
+
+
 void entrada_numeros (int *num1, int *num2, int *crp) {
     unsigned long long chavePublica = 0, chavePrivada; 
     int  p,q,validadeP, validadeQ, validadeCoprimo, numRelativamentePrimo;
